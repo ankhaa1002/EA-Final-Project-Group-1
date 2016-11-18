64e9f82a7d9aae4e4b08bb2143ba6548dd95e0ea
ankhaa1002 <ankhaa1002@gmail.com>
2016-11-18 14:38:52 -0600

    Creation of project

---
+++ mum_volunteer_website/src/main/java/com/mum/MumVolunteerWebsiteApplication.java
@@ -0,0 +1,12 @@
+package com.mum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MumVolunteerWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MumVolunteerWebsiteApplication.class, args);
+	}
+}
