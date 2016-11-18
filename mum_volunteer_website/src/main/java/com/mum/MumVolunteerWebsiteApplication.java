a343511761286a4032be09bc055235e4bcfd1630
bineshsah <binesh.sah@gmail.com>
2016-11-18 16:03:05 -0600

    making sure git is working

--- mum_volunteer_website/src/main/java/com/mum/MumVolunteerWebsiteApplication.java
+++ mum_volunteer_website/src/main/java/com/mum/MumVolunteerWebsiteApplication.java
@@ -8,5 +8,6 @@ public class MumVolunteerWebsiteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MumVolunteerWebsiteApplication.class, args);
+		System.out.println("Making sure git is working??");
 	}
 }
